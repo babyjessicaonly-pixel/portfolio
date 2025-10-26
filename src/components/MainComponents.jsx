e8330a6e11463d47d132bb0841fa315b48fe6cfd
lgugliara <lorenzogugliara.it@gmail.com>
2025-10-26 20:19:15 +0100

    Refactor Header and Footer components for improved structure and accessibility

--- src/components/MainComponents.jsx
+++ src/components/MainComponents.jsx
@@ -1,11 +1,11 @@
+import { useMemo } from "react";
+
 export function Header() {
   return (
     <nav className="navbar bg-dark border-bottom border-body">
       <div className="container-fluid flex-column">
         <figure className="navbar-brand text-center mb-0">
-          <blockquote className="blockquote">
-            <p>Portfolio</p>
-          </blockquote>
+          <p className="lead">Portfolio</p>
           <figcaption className="blockquote-footer mb-0">
             <cite>Rachele Pelliccioni</cite>
           </figcaption>
@@ -16,19 +16,88 @@ export function Header() {
 }
 
 export function Footer() {
+  const appName = useMemo(() => "Rachele Pelliccioni", []);
+  const appDescription = useMemo(() => "Portfolio realizzato con tutto l'amore del mondo e con: Bootstrap, React, Vite, due esseri umani e un'intelligenza artificiale.", []);
+  const linksHeader = useMemo(() => {
+    return "Esplora le sezioni";
+  }, []);
+  const linksContent = useMemo(() => {
+    return [
+      { href: "/la-casa-degli-artisti", label: "La Casa degli Artisti" },
+      { href: "/personaggi-allucinati", label: "Personaggi Allucinati" },
+      { href: "/mokeups", label: "Mokeups" },
+    ];
+  }, []);
+
+  const mainColumn = useMemo(() => {
+    return (
+      <>
+        <a className="d-inline-flex align-items-center text-decoration-none" href="/" aria-label="Bootstrap">
+          <p className="lead">{appName}</p>
+        </a>
+
+        <ul className="list-unstyled small">
+
+          {/* Descrizione */}
+          <li>
+            {appDescription}
+          </li>
+
+          {/* Icone */}
+          <li className="my-4">
+            <a className="icon-link icon-link-hover mx-2 lead" href="https://github.com/babyjessicaonly-pixel/"><i className="bi bi-github"></i></a>
+          </li>
+
+          {/* Licenza */}
+          <li>
+            Code licensed <a className=" text-decoration-none" href="https://github.com/babyjessicaonly-pixel/portfolio/blob/main/LICENSE" target="_blank" rel="license noopener">MIT</a>, docs <a className=" text-decoration-none" href="https://creativecommons.org/licenses/by/4.0/" target="_blank" rel="license noopener">CC BY 4.0</a>.
+          </li>
+
+          {/* Copyright e Anno */}
+          <p>© 2025</p>
+
+        </ul>
+      </>
+    );
+  }, [appDescription, appName]);
+
   return (
-    <nav className="navbar bg-dark">
-      <div className="container-fluid">
+    <footer className="bd-footer px-4 py-5 bg-body-tertiary text-body-secondary" style={{ filter: "grayscale(1)" }}>
+      <div className="container">
+        <div className="row gap-5">
 
-        <div className="col-md-3 text-center">
-          <span className="lead">Rachele Pelliccioni</span>
+          {/* Prima Colonna (Desktop) */}
+          <div className="d-none d-lg-block col-lg-3 border-end">
+            {mainColumn}
+          </div>
+
+          {/* Prima Colonna (Mobile) */}
+          <div className="d-lg-none col-lg-3">
+            {mainColumn}
+          </div>
+
+          {/* Seconda Colonna */}
+          <div className="col col-lg-6">
+
+            {/* Esplora le sezioni */}
+            <p className="lead">{linksHeader}</p>
+
+            {/* Link Sezioni */}
+            <ul className="list-unstyled">
+              {linksContent.map((link, index) => (
+                <li className="mb-2" key={index}>
+                  <a className="icon-link icon-link-hover text-decoration-none align-items-baseline" href={link.href}>
+                    <i className="bi bi-arrow-right-short"></i>
+                    {link.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+
+          </div>
+
         </div>
-
-        <div className="col text-start">
-          <span className="">© 2024</span>
-        </div>
-
       </div>
-    </nav>
+    </footer>
   );
 }
