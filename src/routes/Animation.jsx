a152c85c7fe6c6819a4aa062b74a67425b90c53b
pixel <babyjessicaonly@gmail.com>
2025-11-23 20:11:31 +0100

    cose

--- src/routes/Animation.jsx
+++ src/routes/Animation.jsx
@@ -4,6 +4,7 @@ import AnimationFanculo from '../components/Animation/AnimationFanculo';
 export default function Animation() {
   const primaryText = "Animation";
   const hero = "Prima ancora della passione per i mockup mi ha affascinato il mondo dell'animazione.";
+  const blurp = "La storia di una pesce palla che si gonfia fino a esplodere per difendersi da un mondo troppo stretto, fatto di pressioni, ruoli e ansie quotidiane. Fanculo è un corto d’animazione che trasforma il disagio e la frustrazione in immagini ironiche e autentiche."
 
   return (
     <>
@@ -15,12 +16,26 @@ export default function Animation() {
       <HeroNoCall secondaryText={primaryText} descriptionPrimary={hero} />
 
 
-      {/* Profumo disegno e mockup */}
+      {/* Fanculo presenta... */}
       <div className="w-100 p-4" style={{ backgroundColor: "rgba(101, 185, 115, 1)" }}>
         <div className="container d-flex justify-content-between align-items-start" style={{ maxWidth: "60rem" }}>
-          <img src="/mockup/1.profumo/bona.jpg" className="img-fluid rounded float-start" alt="ops"
+          <img src="/fanculo/episodio1.gif" className="img-fluid rounded float-start" alt="ops"
             style={{ width: "48%", marginRight: "4rem", borderRadius: "8px" }} />
-          <img src="/mockup/1.profumo/mockup_profumo.jpg" className="img-fluid rounded float-end" alt="ops"
+          <HeroNoCall descriptionSecondary={blurp}/>
+        </div>
+      </div>
+
+        {/* Personaggi Gif */}
+        <div className="col-md-10 my-auto">
+          <img src="/fanculo/personaggi/lei_esplode.gif" className="border img-fluid rounded-4 shadow" alt="bob"
+           style={{ width: "48%", marginLeft: "5rem", borderRadius: "10px" }} />
+        </div>
+
+        <div className="w-100 p-4">
+        <div className="container d-flex justify-content-between align-items-start" style={{ maxWidth: "60rem" }}>
+          <img src="/fanculo/personaggi/lei_parla.gif" className="img-fluid rounded float-start" alt="ops"
+            style={{ width: "48%", marginRight: "4rem", borderRadius: "8px" }} />
+          <img src="/fanculo/personaggi/zolo.gif" className="img-fluid rounded float-end" alt="ops"
             style={{ width: "48%", borderRadius: "8px" }} />
         </div>
       </div>
